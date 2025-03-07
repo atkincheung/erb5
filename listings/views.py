35fb812034741de8371d0df7c591e16092b8e56e
tommy <atkincheung@gmail.com>
2025-03-07 17:52:00 +0800

    Step 7:template

--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from . models import Listing
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def listings(request):
-    #listings = Listing.objects.all()
+    listings = Listing.objects.all()
+   # for listing in listings:
+   #     print(listing)
+    #print(list(listings)) 
+   # listings= listing.objects.filter 
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)
     paginator=Paginator(listings,3) 
     page = request.GET.get('page')
@@ -13,7 +17,9 @@ def listings(request):
     return render(request, 'listings/listings.html', context)
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    listing = get_object_or_404(Listing, pk=listing_id)
+    context = {'listing': listing}
+    return render(request, 'listings/listing.html',context)
 
 def search(request):
     return render(request, 'listings/search.html')
