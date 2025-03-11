4d4578cea709f1817c1fb5356562ce289184f17a
tommy <atkincheung@gmail.com>
2025-03-11 11:23:46 +0800

    Step 8 update choice

---
+++ listings/choices.py
@@ -0,0 +1,47 @@
+bedroom_choices = {
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    '10': 10
+}
+
+price_choices = {
+    '100000': '$1,000,000',
+    '200000': '$2,000,000',
+    '300000': '$3,000,000',
+    '400000': '$4,000,000',
+    '500000': '$5,000,000',
+    '600000': '$6,000,000',
+    '700000': '$7,000,000',
+    '800000': '$8,000,000',
+    '900000': '$9,000,000',
+    '1000000': '$10M+',
+}
+
+district_choices = {
+    "Islands": 'Islands',
+    "Kwai Tsing": 'Kwai Tsing',
+    "Sai Kung": 'Sai Kung',
+    "Tsuen Wan": 'Tsuen Wan',
+    "Tuen Mun": 'Tuen Mun',
+    "Yuen Long": 'Yuen Long',
+    "Wong Tai Sin": 'Wong Tai Sin',
+    "Sha Tin": 'Sha Tin',
+    "Tai Po": 'Tai Po',
+    "Kowloon City": 'Kowloon City',
+    "Kwun Tong": 'Kwun Tong',
+    "Sham Shui Po": 'Sham Shui Po',
+    "Yau Tsim Mong": 'Yau Tsim Mong',
+    "Central and Western": 'Central and Western',
+    "Eastern": 'Eastern',
+    "Southern": 'Southern',
+    "Wan Chai": 'Wan Chai',
+    "North": 'North',
+    
+    }
--- listings/views.py
+++ listings/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from . models import Listing
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-
+from django.db.models import Q,F
+from listings.choices import price_choices, bedroom_choices, district_choices
 # Create your views here.
 def listings(request):
-    listings = Listing.objects.all()
+   # listings = Listing.objects.all()
    # for listing in listings:
    #     print(listing)
     #print(list(listings)) 
    # listings= listing.objects.filter 
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+   # listings = Listing.objects.filter(Q(district='tst') | ~Q(district='mk'))
+  #  listings = Listing.objects.filter(district=F('address'))
     paginator=Paginator(listings,3) 
     page = request.GET.get('page')
     paged_listings = paginator.get_page(page)
