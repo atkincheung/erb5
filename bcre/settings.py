7ad3dda38bd6a51cb4d316709f711eb480c5a0ca
tommy <atkincheung@gmail.com>
2025-03-14 17:46:48 +0800

    Step 12 dashboard

--- bcre/settings.py
+++ bcre/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os 
 from pathlib import Path
-
+from django.contrib.messages import constants as messages
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
     'accounts.apps.AccountsConfig',
+    'contacts.apps.ContactsConfig',
 ]
 
 MIDDLEWARE = [
@@ -145,3 +146,8 @@ INTERNAL_IPS = [
 # media
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',#use bootstrap class
+    messages.SUCCESS: 'success'
+}
